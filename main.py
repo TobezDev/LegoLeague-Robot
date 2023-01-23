be35301bfc0bebf72211bf8a93b4072df61eacc8
TheRealTobes28-Codez <83095177+TheRealTobes28-Codez@users.noreply.github.com>
2023-01-23 13:16:23 +0000

    Initial Comitt (main.py)

---
+++ main.py
@@ -0,0 +1,28 @@
+from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
+from spike.control import wait_for_seconds, wait_until, Timer
+from math import *
+
+
+hub = PrimeHub()
+
+
+def setup():
+    Hub = PrimeHub()
+    StatusLight = Hub.status_light
+    LightMatrix = Hub.light_matrix
+
+    distanceSensor = DistanceSensor('<port>')
+    colorSensor = ColorSensor('<port>')
+    motor_r = Motor('<port>')
+    motor_l = Motor(\'<port>')
+
+    motors = MotorPair(motor_l, motor_r)
+
+
+def startup():
+    StatusLight.on(color='orange')
+    setup()
+
+@hub.event
+async def on_ready():
+    startup()
