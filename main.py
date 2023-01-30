d9047d828002272b722cca58f450387d7040c7c2
TobezDev <83095177+TobezDev@users.noreply.github.com>
2023-01-30 08:01:39 +0000

    fix main.py

--- main.py
+++ main.py
@@ -2,30 +2,20 @@ from spike import PrimeHub, MotorPair, Motor, ColorSensor
 
 hub = PrimeHub()
 
-
-def log(text):
-    try:
-        print(f"[LOGS]: {text}")
-    except SyntaxError as err:
-        print(f"[SyntaxError]: {err}")
-    except TypeError as err:
-        print(f"[TypeError]: {err}")
-
-
 movement_motors = MotorPair("F", "E")
+picker_motor = Motor("D")
+color_sensor = ColorSensor("C")
+
 movement_motors.set_default_speed(70)
 
-picker_motor = Motor("D")
-
-colour_sensor = ColorSensor("C")
-colour_sensor.light_up(100, 100, 100)
+color_sensor.light_up(100, 100, 100)
 
 picker_motor.run_to_position(0)
 movement_motors.move(30)
+
 movement_motors.start()
 
-colour_sensor.wait_until_color("black")
-log("Road Found")
+color_sensor.wait_until_color("black")
 
 movement_motors.stop()
 movement_motors.move(10, "cm", -90)
